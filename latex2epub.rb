2a4a01a2d4148b6593d9c7c90c662a6381795d4d
Kenshi Muto <kmuto@debian.org>
2010-12-19 09:56:50 +0900

    license

--- latex2epub.rb
+++ latex2epub.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/ruby
 # encoding: utf-8
+#
+# Copyright (c) 2010 Kenshi Muto
+#
+# This program is free software.
+# You can distribute or modify this program under the terms of
+# the GNU LGPL, Lesser General Public License version 2.1.
+# For details of the GNU LGPL, see the file "COPYING".
 
 $KCODE='u'
 
